d37befd451ea4f1e26697893118ddf05aa6d9306
Anthony Williams <antgwms@outlook.com>
2020-08-17 23:19:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
+    }
+}
