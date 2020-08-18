85a2a31d78f3e67d0c9288e67e0b05dc5173fd8c
Anthony Williams <antgwms@outlook.com>
2020-08-19 00:11:03 +0100

    onClick Toast
    add onClick to the Toast button

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hellotoast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                                                        Toast.LENGTH_SHORT;
+        toast.show();
+
     }
 
     public void countUp(View view) {
