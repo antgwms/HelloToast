9bc91e6902535d85817be3aa6ea829873115e8f7
Anthony Williams <antgwms@outlook.com>
2020-08-21 23:51:25 +0100

    Fix Count Button
    Added a resource for the string "button_toast" to fix button issue

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message,
-                                                        Toast.LENGTH_SHORT;
+                                                    Toast.LENGTH_SHORT);
         toast.show();
 
     }
